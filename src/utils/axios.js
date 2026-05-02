cfe5761f02b0aad5e3e22cb4163942b6b0345009
Akhilesh027 <126266564+akhileshreddy027@users.noreply.github.com>
2026-05-02 21:27:48 +0530

    cdsz

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,29 +10,6 @@ const axiosInstance = axios.create({
   },
 });
 
-// Request interceptor: attach token if present
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("vendorToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Response interceptor: handle 401 unauthorized globally
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("vendorToken");
-      localStorage.removeItem("vendor");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
+// No interceptors – authentication has been removed
 
 export default axiosInstance;
